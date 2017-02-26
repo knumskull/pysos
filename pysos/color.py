cba895f3c1209ec8cbb289502571f7566684217d
Steffen Froemer <steffen@redhat.com>
2017-02-26 14:04:37 +0100

    minor PEP8 improvements, replaced getLastUpdate with a more efficient solution

--- pysos/color.py
+++ pysos/color.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 class Color:
 
     def __init__(self):
--- pysos/yum.py
+++ pysos/yum.py
@@ -1,13 +1,15 @@
+# -*- coding: utf8 -*-
 import sys
 import os
-from . import pysosutils
-import re
-from .colors import *
+import pysosutils
+from color import Color
+
 
 class Object(object):
     pass
 
-class yum():
+
+class YUM:
     """ 
     Capture and optionally display yum and subscription data
     for RPM based systems.
@@ -21,13 +23,12 @@ class yum():
 
     def getLastUpdate(self):
         """ Get the last package updated """
-        if os.path.isfile(self.target + 'var/log/yum.log'):
-            with open(self.target + 'var/log/yum.log', 'r') as yfile:
-                # this is horribly ineffecient. Need a better way.  
-                lines = yfile.readlines()
+        if os.path.isfile(os.path.join(self.target, 'var/log/yum.log')):
+            with open(self.target + 'var/log/yum.log', 'rb') as yfile:
                 try:
-                    return lines[-1]
-                except IndexError:
+                    yfile.seek(-256, os.SEEK_END)
+                    return yfile.readlines()[-1].decode()
+                except (IndexError, IOError):
                     return False
 
         else:
@@ -39,7 +40,7 @@ class yum():
         if lastUpdate:
             return lastUpdate[:15]
         else:
-            return ' Unknown'
+            return 'Unknown'
 
     def getRepoList(self):
         """ Compile a list of all repos the system is using """
@@ -66,29 +67,23 @@ class yum():
 
     def getSubMgrInst(self):
         """ Get subscription information from subscription manager """
-        if os.path.isfile(self.target + 
-            'sos_commands/general/subscription-manager_list_--installed'):
+        if os.path.isfile(self.target + 'sos_commands/general/subscription-manager_list_--installed'):
             prodToParse = []
             prodInfo = []
-            with open(self.target + 
-                    'sos_commands/general/subscription-manager_list_--installed',
-                    'r') as sfile:
+            with open(self.target + 'sos_commands/general/subscription-manager_list_--installed', 'r') as sfile:
                 for line in sfile:
                     if line.startswith('Product Name:'):
                         prod = Object()
                         prod.header = line.strip('\n')
                         prodToParse.append(prod)
             for prod in prodToParse:
-                 prod.data = pysosutils.parseOutputSection(
-                    self.target + 
-                    'sos_commands/general/subscription-manager_list_--installed',
-                    prod.header)
-                 for k in prod.data:
-                     setattr(prod, str(k).lower().strip().replace(
-                                                ' ', ''), prod.data[k])
-                 prod.name = prod.header.replace('Product Name:',
-                                                            '').strip()
-                 prodInfo.append(prod)
+                prod.data = pysosutils.parseOutputSection(self.target +
+                                                          'sos_commands/general/subscription-manager_list_--installed',
+                                                          prod.header)
+                for k in prod.data:
+                    setattr(prod, str(k).lower().strip().replace(' ', ''), prod.data[k])
+                prod.name = prod.header.replace('Product Name:', '').strip()
+                prodInfo.append(prod)
             return prodInfo
         else:
             return False
@@ -97,29 +92,27 @@ class yum():
         """ Display gathered yum related information """
         yumInfo = self.getRepoList()
         lastUpdate = self.getLastUpdateDate()
-        print(colors.BSECTION + "Package Information"\
-            + colors.ENDC)
-        print(colors.BHEADER + '\t Plugins     : ' + colors.ENDC + \
-            self.plugins)
+        colors = Color()
+        print(colors.BSECTION + "Package Information" + colors.ENDC)
+        print(colors.BHEADER + '\t Plugins     : ' + colors.ENDC + self.plugins)
 
         if lastUpdate:
-            print(colors.BHEADER + '\t Last Update :  ' + \
-                colors.ENDC + lastUpdate)
+            print(colors.BHEADER + '\t Last Update :  ' + colors.ENDC + lastUpdate)
         print(colors.BHEADER + '\t Repos       : ' + colors.ENDC)
         if yumInfo:
             for repo in yumInfo:
                 print('\t\t\t' + repo.repo)
         else:
-            print('\t\t\t ' + colors.BRED + 'Repolist file not found'\
-                    + colors.ENDC)
+            print('\t\t\t ' + colors.BRED + 'Repolist file not found' + colors.ENDC)
 
     def displaySubInfo(self):
         """ Display subscription related information """
         prodInfo = self.getSubMgrInst()
+        colors = Color()
         print('')
         if prodInfo:
-            print(colors.BHEADER + '\t Products    :  ' + \
-                colors.ENDC + str(len(prodInfo)))
+            print(colors.BHEADER + '\t Products    :  ' +
+                  colors.ENDC + str(len(prodInfo)))
             for prod in prodInfo:
                 print('\t\t\t' + prod.name)
                 print('\t\t\t' + prod.version + ' ' + prod.arch)
@@ -135,5 +128,5 @@ class yum():
 
 if __name__ == '__main__':
     target = sys.argv[1]
-    test = yum(target)
+    test = YUM(target)
     test.displayAllYumInfo()
